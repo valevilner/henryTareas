6aa0c0a6acb7d9196c7e8b577e34778532f0385c
valevilner <vaale-3@hotmail.com>
2022-05-05 09:09:11 -0300

    realizada

--- 07-Algoritmos-II/homework/homework.js
+++ 07-Algoritmos-II/homework/homework.js
@@ -1,4 +1,5 @@
-'use strict'
+'use strict';
+
 // No cambies los nombres de las funciones.
 
 function quickSort(array) {
@@ -7,6 +8,39 @@ function quickSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  let aux = array.length;
+  if (aux <= 1) {
+    return array;
+  }
+  let pivote = array[0];
+  let left = [];
+  let right = [];
+
+  for (let i = 1; i < aux; i++) {
+    if (array[i] < pivote) {
+      left.push(array[i]);
+    } else {
+      right.push(array[i]);
+    }
+  }
+  return quickSort(left).concat(pivote, quickSort(right));
+}
+
+function merge(left, right) {
+  let izq = 0;
+  let der = 0;
+  let array = [];
+
+  while (izq < left.length && der < right.length) {
+    if (left[izq] < right[der]) {
+      array.push(left[izq]);
+      izq++;
+    } else {
+      array.push(right[der]);
+      der++;
+    }
+  }
+  return array.concat(left.slice(izq)).concat(right.slice(der));
 }
 
 function mergeSort(array) {
@@ -15,6 +49,13 @@ function mergeSort(array) {
   // Devolver el array ordenado resultante
   // Tu código:
 
+  if (array.length === 1) return array;
+
+  let mitad = Math.floor(array.length / 2);
+  let left = array.slice(0, mitad);
+  let right = array.slice(mitad);
+
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 // No modificar nada debajo de esta línea
